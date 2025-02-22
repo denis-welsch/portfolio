7f169bb0c4e112156afa48eb9048c86494dd6597
Denisator82 <denis.welsch@gmx.de>
2025-02-22 14:57:24 +0100

    section portfolio

--- src/app/main-content/portfolio/portfolio.component.ts
+++ src/app/main-content/portfolio/portfolio.component.ts
@@ -8,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.component.scss'
 })
 export class PortfolioComponent {
+    projects = [
+        {
+            id: 1,
+            title: 'Join',
+            description: 'Task manager inspired by the Kanban System...',
+            technologies: ['Angular', 'TypeScript', 'HTML', 'CSS', 'Firebase'],
+            image: 'assets/images/join.png',
+            github: 'https://github.com/yourrepo/join',
+            live: 'https://yourliveapp.com/join'
+        },
+        {
+            id: 2,
+            title: 'El Pollo Loco',
+            description: 'Jump and run game based on object-oriented programming...',
+            technologies: ['HTML', 'CSS', 'JavaScript'],
+            image: 'assets/images/el_pollo_loco.png',
+            github: 'https://github.com/yourrepo/elpollo',
+            live: 'https://yourliveapp.com/elpollo'
+        },
+        {
+            id: 3,
+            title: 'DABubble',
+            description: 'A Slack Clone for team collaboration...',
+            technologies: ['Angular', 'Firebase', 'TypeScript'],
+            image: 'assets/images/dabubble.png',
+            github: 'https://github.com/yourrepo/dabubble',
+            live: 'https://yourliveapp.com/dabubble'
+        }
+    ];
 
+    hoveredProject: any = null;
+    selectedProject: any = null;
+
+    hoverProject(project: any) {
+        this.hoveredProject = project;
+    }
+
+    openProject(project: any) {
+        this.selectedProject = project;
+    }
+
+    closeProject() {
+        this.selectedProject = null;
+    }
 }
