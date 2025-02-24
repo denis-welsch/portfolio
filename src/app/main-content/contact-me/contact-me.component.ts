0fc03d16d2a252cfaca5ea6376adac6589b3b7f2
Denisator82 <denis.welsch@gmx.de>
2025-02-24 13:10:19 +0100

    section contactMe from TS config

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
@@ -10,6 +11,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class ContactMeComponent {
 
+  http = inject(HttpClient);
+
   contactData = {
     name: "",
     email: "",
@@ -17,8 +20,36 @@ export class ContactMeComponent {
     privacyAccepted: false
   };
 
-  onSubmit(){
-    console.log(this.contactData)
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
   };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
 
 }
