2d56eb7ee7381294b49926fccc8de292a24d1db0
Denisator82 <denis.welsch@gmx.de>
2025-02-24 12:41:24 +0100

    section contactMe form

--- src/app/main-content/contact-me/contact-me.component.ts
+++ src/app/main-content/contact-me/contact-me.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
+
+  contactData = {
+    name: "",
+    email: "",
+    message: "",
+    privacyAccepted: false
+  };
+
+  onSubmit(){
+    console.log(this.contactData)
+  };
 
 }
