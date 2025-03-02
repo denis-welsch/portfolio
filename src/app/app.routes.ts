c5ad68778823a9d4da5fce0209fa5bf0d37c4a9f
Denisator82 <denis.welsch@gmx.de>
2025-03-02 17:11:51 +0100

    imprint & privacy-policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component'; // Neue Komponente importieren
 
 export const routes: Routes = [
-    {path:'',component: MainContentComponent},
-    {path:'imprint', component: ImprintComponent}
+    { path: '', component: MainContentComponent },
+    { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent } // Route für Datenschutzerklärung hinzufügen
 ];
+
