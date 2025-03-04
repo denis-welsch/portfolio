79d51d4f94ceb535f6d07d23350b70e8544b65c1
Denisator82 <denis.welsch@gmx.de>
2025-03-04 18:51:36 +0100

    design changes hero,aboutMe, references, skills

--- src/app/main-content/about-me/about-me.component.ts
+++ src/app/main-content/about-me/about-me.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import {TranslatePipe, TranslateDirective} from "@ngx-translate/core";
+import { TranslatePipe, TranslateDirective } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
   imports: [TranslatePipe, TranslateDirective],
   templateUrl: './about-me.component.html',
-  styleUrl: './about-me.component.scss'
+  styleUrls: ['./about-me.component.scss']
 })
 export class AboutMeComponent {
+  // Variable zum Steuern des Hover-Zustands
+  isHovered: boolean = false;
+
+  // Methode, die den Hover-Zustand ändert
+  onHover(state: boolean): void {
+    this.isHovered = state;
+  }
 }
