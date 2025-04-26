6927eac6d2035d41d1b7567d640f9bbcc926dd25
Denisator82 <denis.welsch@gmx.de>
2025-04-26 10:15:00 +0200

    footer link imprint

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,13 +1,24 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
+  /**
+   * Creates an instance of ImprintComponent.
+   * @param location The Angular Location service for interacting with the browser's history.
+   */
+  constructor(private location: Location) { }
 
+  /**
+   * Navigates the user back to the previous page in the browser's history.
+   */
+  backPage() {
+    this.location.back();
+  }
 }
--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,13 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-footer',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+    selector: 'app-footer',
+    standalone: true,
+    imports: [RouterModule],
+    templateUrl: './footer.component.html',
+    styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
 }
