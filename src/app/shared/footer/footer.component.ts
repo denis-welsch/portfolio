f88a1e539412d6dc6c2192181275c24650ec5403
Denisator82 <denis.welsch@gmx.de>
2025-04-30 17:28:10 +0200

    little changes after feedback

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateDirective, TranslatePipe } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-footer',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, TranslateDirective, TranslatePipe],
     templateUrl: './footer.component.html',
     styleUrl: './footer.component.scss'
 })
